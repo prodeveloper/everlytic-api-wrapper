3927a7186b4bfe8483d2388e9d07c4735b440beb
Chencha Jacob <jmokua@gmail.com>
2014-05-21 19:46:08 +0300

    added sample config file

--- src/config/api.php
+++ src/config/api.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'user'=>'trans_ihub_034',
-    'url'=>'http://ihub.everlytic.net',
-    'apiKey'=>'w1QO9QhPwX0Bt74nRNemIzfXs1TUmY8s'
+    'user'=>'',
+    'url'=>'',
+    'apiKey'=>''
 );
